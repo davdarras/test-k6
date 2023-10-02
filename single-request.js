5c968ff712d8e9c8980553aee7c557829e8c8faa
David Darras <117278497+davdarras@users.noreply.github.com>
2023-10-02 18:22:52 +0200

    Update single-request.js

--- single-request.js
+++ single-request.js
@@ -2,14 +2,14 @@ import http from "k6/http";
 import { sleep } from "k6";
 
 export const options = {
-  iterations: 2,
+  iterations: 5,
 };
 
 export default function () {
-  let resp = http.get("http://queen-api:8080/api/survey-unit/11");
-  console.log(`status: "${resp.body}"`);
-  //http.get("http://queen-api:8080/api/survey-unit/42");
-  //http.get("http://queen-api:8080/api/survey-unit/12");
-  //sleep(3);
-  //http.get("http://queen-api:8080/api/survey-unit/11");
+  let resp = http.get("http://queen-api:8080/api/survey-unit/21");
+  //console.log(`status: "${resp.body}"`);
+  http.get("http://queen-api:8080/api/survey-unit/22");
+  http.get("http://queen-api:8080/api/survey-unit/23");
+  sleep(3);
+  http.get("http://queen-api:8080/api/survey-unit/24");
 }
