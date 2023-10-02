d6631765a9678b6e3cbbe911b58a2b85e51f16da
David Darras <117278497+davdarras@users.noreply.github.com>
2023-10-02 18:10:55 +0200

    Update single-request.js

--- single-request.js
+++ single-request.js
@@ -7,7 +7,7 @@ export const options = {
 
 export default function () {
   let resp = http.get("http://queen-api:8080/api/survey-unit/11");
-  console.log(`status: "${resp.status}"`);
+  console.log(`status: "${resp.body}"`);
   //http.get("http://queen-api:8080/api/survey-unit/42");
   //http.get("http://queen-api:8080/api/survey-unit/12");
   //sleep(3);
